5cc6e9a759e14ef2e14d8698ec1b7549a9ab3cf9
ezcn <enza.colonna@gmail.com>
2017-07-13 22:22:48 +0200

    update

--- NeFromLD/Ne_LD.py
+++ NeFromLD/Ne_LD.py
@@ -16,9 +16,9 @@ usage:   python Ne_LD.py infile n_haplo """
 infile = sys.argv[1]
 n_haplo = sys.argv[2] # number of chromosomes used to calculate ld 
 min_nb_observ_in_bin = 2 # minimum nuber of observations in a bin 
-maxdist_cM = 20  # 0.5 max distance in cM between marker pairs 
-step_cM = 0.01   #0.005 increment in cM for bins 
-minr2 = 0.00001 # minimun r2 to be considered  
+maxdist_cM = 0.25  # 0.5 max distance in cM between marker pairs 
+step_cM = 0.005	   #0.005 increment in cM for bins 
+minr2 = 0.00 # minimun r2 to be considered  
 maxr2 = 0.9999 # maximum r2 to be considered
 
 
@@ -55,8 +55,8 @@ def t_estim (morg_dist):
 	
 
 #''''''''''''  define bins of distance in cM 
-
-bins = [i for i in numpy.arange (0, maxdist_cM, step_cM )]
+maxdist_M = maxdist_cM/100.0 ; step_M = step_cM/100.0 # convert intervals in Morgan
+bins = [i for i in numpy.arange (0, maxdist_M, step_M )]
 binsnames = [i for i in range(1, len(bins) ) ]
 #binsnames =map(chr, range(65, 65+len(bins)-1 ))   
 
@@ -67,7 +67,7 @@ with open (infile, "r") as f:
 	next(f) 
 	for line in f: 	
 		x = line.split()
-		indist = float(x[0]); inr2=float(x[1])
+		indist = float(x[0])/100.0 ; inr2=float(x[1]) # store distance in Morgans 
 		if minr2 <= inr2 <= maxr2:  
 			raw_data['distance'].append(indist) 
 			raw_data['r2'].append(inr2)
@@ -87,19 +87,21 @@ title=['bin','nb_marker_pairs' ,  'average_r2', 'average_distance_cM' , 'time_ge
 print "\t".join(title)
 
 for b in binsnames:
-	#print '###'
 	r2set = df.loc[df['categories']==b]['r2'] # select all r2 values in the bin 
 	distset = df.loc[df['categories']==b]['distance']
-	#print '>>>', b, bins_data[b] 
-	#print r2set 
+	#print "########" 
+	#print distset 
+
 	if len(r2set) >= min_nb_observ_in_bin: 
 		meanr2 = numpy.mean(r2set) # average r2 in bin 
-		bin_distance = numpy.mean(distset)/100.0 ## average distance in Morgan in the bin		
-
-		if bin_distance >0 : 
+		#print '~~~~~~~~~~'
+		#print numpy.mean(distset)  , ' mean dist' 
+		bin_distance = numpy.mean(distset)   # average distance in Morgan in the bin		
+		#print bin_distance,  ' bin dist'
+		if bin_distance >0 :  # if genetic distance is 0M estimates are not possibles  
 			time = t_estim( bin_distance) 
 			ne_corr = ne_estim_corr(meanr2, bin_distance , n_haplo)  
 			ne = ne_estim(meanr2, bin_distance)  
 
-			res=[b, len(r2set) , meanr2, bin_distance, time, ne, ne_corr ]
+			res=[b, len(r2set) , meanr2, bin_distance*100, time, ne, ne_corr ]
         		print "\t".join(map(str, res))
